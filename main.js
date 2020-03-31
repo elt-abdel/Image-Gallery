a8caab35a7b18616ec2cc159e12c29eb3b2d81f1
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-03-31 18:18:34 -0400

    looping through images

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
+for (let i = 0; i < 5; i++) {
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", "images/pic" + i + ".jpg");
+  thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
