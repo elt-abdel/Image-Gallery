e02c320d5ee08cf1dbffa95234370dca9f289cc0
parasdev123 <45490064+parasdev123@users.noreply.github.com>
2020-03-31 18:19:54 -0400

    fixed the loop index error

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
-for (let i = 0; i < 5; i++) {
+for (let i = 1; i < 6; i++) {
   const newImage = document.createElement("img");
   newImage.setAttribute("src", "images/pic" + i + ".jpg");
   thumbBar.appendChild(newImage);
