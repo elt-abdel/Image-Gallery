99a287ca977ac7c72b8731f1a3bcc881357fbdc8
Eltayeb Abdelrahman <52611456+elt-abdel@users.noreply.github.com>
2020-04-01 14:48:55 -0400

    btn Handler

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
+var btnAttribute = btn.getAttribute('class');
+
 /* Looping through images */
 for (let i = 1; i < 6; i++) {
   const newImage = document.createElement("img");
@@ -17,5 +19,19 @@ for (let i = 1; i < 6; i++) {
 
   function onclickFunction() {
     displayedImage.src = newImage.src;
+  }
+}
+
+// Handler for Dark/Light btn
+// if classname = dark change to light and and textcontent to lighten and bg color to rgb(0,0,0,0.5)
+btn.onclick = function() {
+  onclickBtn();
+};
+
+function onclickBtn() {
+  if (btnAttribute == 'dark'){
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = (0,0,0,0.5);
   }
 }
